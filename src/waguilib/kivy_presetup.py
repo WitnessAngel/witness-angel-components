11bfbb65c6217580ad1f6caa63f517813cca7a37
Pascal Chambon <pythoniks@gmail.com>
2021-04-08 21:38:40 +0200

    Gather Kivy Window initialization code

--- src/waguilib/kivy_presetup.py
+++ src/waguilib/kivy_presetup.py
@@ -21,6 +21,25 @@ try:
 
     # import logging_tree
     # logging_tree.printout()
+
+    #if sys.platform == "win32":
+    #    os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
+
 except Exception as exc:
     print(">>>>>>>> FAILED INITIALIZATION OF WA GUI LOGGING: %r" % exc)
 
+
+# SETUP INITIAL STATE OF THE WINDOW
+try:
+    from kivy.config import Config
+    Config.set('graphics', 'top', '50')
+    Config.set('graphics', 'left', '50')
+    Config.set('graphics', 'position', 'custom')
+    # FIXME this happens too late I guess
+    #Config.set("graphics", "fullscreen", "0")
+    #Config.set("graphics", "show_cursor", "1")
+
+    from kivy.core.window import Window
+    Window.minimum_width, Window.minimum_height = Window.size = (500, 380)
+except Exception as exc:
+    print(">>>>>>>> FAILED INITIALIZATION OF WA GUI WINDOW: %r" % exc)
