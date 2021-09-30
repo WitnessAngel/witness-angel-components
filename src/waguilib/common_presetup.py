b3ea39d10a5819bcb2c4971f4a6be1bb123c3de9
Pascal Chambon <pythoniks@gmail.com>
2021-09-30 18:48:40 +0200

    Auto-patch ctypes on Android

--- src/waguilib/common_presetup.py
+++ src/waguilib/common_presetup.py
@@ -1,3 +1,6 @@
+from waguilib.android_helpers import patch_ctypes_module
+from waguilib.importable_settings import IS_ANDROID
+
 
 def setup_generic_app(package):
     """Setup for both gui and console apps."""
@@ -5,3 +8,6 @@ def setup_generic_app(package):
     if os.getenv("WACLIENT_ENABLE_TYPEGUARD"):
         from typeguard.importhook import install_import_hook
         install_import_hook(package)
+
+    if IS_ANDROID:
+        patch_ctypes_module()  # Necessary for wacryptolib
