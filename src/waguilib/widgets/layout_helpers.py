5aa91fcec8dd08801264ebf98ad7456814957bed
Pascal Chambon <pythoniks@gmail.com>
2021-12-13 10:52:09 +0100

    Factorize Scrollview helpers

--- src/waguilib/widgets/layout_helpers.py
+++ src/waguilib/widgets/layout_helpers.py
@@ -1,46 +1,25 @@
-
+import textwrap
+from pathlib import Path
 
 from kivy.lang import Builder
 
 
-WIDGET_OUTLINE_HACK = """
-<Widget>:
-    canvas.after:
-        Line:
-            rectangle: self.x+1,self.y+1,self.width-1,self.height-1
-            dash_offset: 5
-            dash_length: 3
-"""
-
-
 def activate_widget_debug_outline():
     """Create a dotted outline around widgets, to help layout setup."""
-    Builder.load_string(WIDGET_OUTLINE_HACK)
+    widget_outline_hack = textwrap.dedent("""
+    <Widget>:
+        canvas.after:
+            Line:
+                rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+                dash_offset: 5
+                dash_length: 3
+    """)
+
+    Builder.load_string(widget_outline_hack)
 
 
 def load_layout_helper_widgets():
-    Builder.load_string("""
-    
-# Other helpers which might be useful
-
-<Separator@Widget>:
-    canvas:
-        Color:
-            rgba: 1, 1, 1, 0
-        Rectangle:
-            pos: self.pos
-            size: self.size
-
-<VSeparator@Separator>:
-    size_hint_y: None
-    height: 20
-
-<HSeparator@Separator>:
-    size_hint_x: None
-    width: 20
-
-    """)
-
+    Builder.load_file(str(Path(__file__).parent / 'layout_helpers.kv'))  # TODO use "with_name"
 
 # FIXME use or remove?
 #<ConsoleOutput>:
