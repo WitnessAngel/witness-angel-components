3899c4b2eba4aa25ebcd28a6d24a0ef89287d15a
Pascal Chambon <pythoniks@gmail.com>
2022-05-05 18:22:24 +0200

    Reload main NVR page when language changes

--- src/wacomponents/screens/recorder_homepage.py
+++ src/wacomponents/screens/recorder_homepage.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from kivy.lang import Builder
+from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 
 from wacomponents.widgets.layout_components import LanguageSwitcherScreenMixin
@@ -10,10 +11,11 @@ Builder.load_file(str(Path(__file__).parent / 'recorder_homepage.kv'))
 
 
 class LauncherWithImagePreviewScreen(LanguageSwitcherScreenMixin, Screen):
-    pass
-    """
-    def __init__(self, **kwargs):
-        self.title = "Witness Angel - WardProject"
 
-        super(MainWindow, self).__init__(**kwargs)
-    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._app = MDApp.get_running_app()
+
+    def language_menu_select(self, lang_code):
+        super().language_menu_select(lang_code)
+        self._app.refresh_checkup_status()  # Refresh translation of Drive etc.
