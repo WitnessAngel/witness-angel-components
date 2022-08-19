1be8de87cbb3a5b8413dbc9215166002295596fe
ATCGeraldine <atchadegeraldine@gmail.com>
2022-08-19 17:17:56 +0200

    CREATE WAScreenName class which lists the name of all screens

---
+++ src/wacomponents/screens/base.py
@@ -0,0 +1,15 @@
+class WAScreenName:
+    # NVR Screen
+    recorder_homepage = "recorder_homepage"
+    foreign_keystore_management = "foreign_keystore_management"
+    cryptainer_storage_management = "cryptainer_storage_management"
+    cryptainer_decryption_process = "cryptainer_decryption_process"
+    claimant_revelation_request_creation_form = "claimant_revelation_request_creation_form"
+    claimant_revelation_request_management = "claimant_revelation_request_management"
+    cryptainer_decryption_result = "cryptainer_decryption_result"
+
+    # AUTHENTICATOR Screen
+    authenticator_management = "authenticator_management"  # ok
+    authenticator_creation_form = "authenticator_creation_form"  # ok
+    authenticator_publication_form = "authenticator_publication_form"  # ok
+    authenticator_revelation_request_management = "authenticator_revelation_request_management"  #
