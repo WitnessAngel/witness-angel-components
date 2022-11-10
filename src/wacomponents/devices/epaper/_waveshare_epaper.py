52a6b364a8a3471e7c95b33cff4192d9c175636d
Pascal Chambon <pythoniks@gmail.com>
2022-11-10 22:10:32 +0100

    Factorize Epaper code thanks to WaveshareEpaperStatusDisplayBase

--- src/wacomponents/devices/epaper/_waveshare_epaper.py
+++ src/wacomponents/devices/epaper/_waveshare_epaper.py
@@ -8,9 +8,26 @@ from wacomponents.devices.epaper._epaper_base import EpaperStatusDisplayBase
 FONT_PATH = str(Path(__file__).parent.joinpath('Font.ttc'))  # FIXME put assets elsewhere
 
 
-class WaveshareEpaperStatusDisplay2in7(EpaperStatusDisplayBase):
+class WaveshareEpaperStatusDisplayBase(EpaperStatusDisplayBase):
 
-    # COMMON PARAMETERS #
+    # Subclass must define self.epd! #
+
+    def _initialize_display(self):
+        self.epd.init()
+
+    def _display_image(self, pil_image):
+        #self._clear_display()
+        pil_image.convert('1')  # Most screens don't support levels of grey
+        self.epd.display(self.epd.getbuffer(pil_image))
+
+    def _clear_display(self):
+        self.epd.Clear(0xFF)
+
+    def _release_display(self):
+        self.epd.sleep()  # IMPORTANT, else the display can be damaged by long power-on
+
+
+class WaveshareEpaperStatusDisplay2in7(WaveshareEpaperStatusDisplayBase):
 
     # INVERTED, since we want landscape orientation!
     PAPER_WIDTH = epd2in7.EPD_HEIGHT
@@ -37,15 +54,10 @@ class WaveshareEpaperStatusDisplay2in7(EpaperStatusDisplayBase):
     def _display_image(self, pil_image):
         self.epd.display_4Gray(self.epd.getbuffer_4Gray(pil_image))
 
-    def _clear_display(self):
-        self.epd.Clear(0xFF)
 
-    def _release_display(self):
-        self.epd.sleep()  # IMPORTANT, else the display can be damaged by long power-on
+class WaveshareEpaperStatusDisplay2in13V3(WaveshareEpaperStatusDisplayBase):
 
-
-class WaveshareEpaperStatusDisplay2in13V3(WaveshareEpaperStatusDisplay2in7):
-
+    # INVERTED, since we want landscape orientation!
     PAPER_WIDTH = epd2in13_V3.EPD_HEIGHT
     PAPER_HEIGHT = epd2in13_V3.EPD_WIDTH
 
@@ -55,20 +67,10 @@ class WaveshareEpaperStatusDisplay2in13V3(WaveshareEpaperStatusDisplay2in7):
     PREVIEW_IMAGE_WIDTH = 120
     PREVIEW_IMAGE_HEIGHT = int(PREVIEW_IMAGE_WIDTH / (16/9))
 
-    BUTTON_PIN_1 = None
-    BUTTON_PIN_2 = None
-    BUTTON_PIN_3 = None
-    BUTTON_PIN_4 = None
+    # No BUTTON_PIN_X on this screen, so leave them as "None"
 
     SMALL_DISPLAY = True
 
     def __init__(self):
         self.epd = epd2in13_V3.EPD()
 
-    def _initialize_display(self):
-        self.epd.init()
-
-    def _display_image(self, pil_image):
-        self._clear_display()
-        pil_image.convert('1')
-        self.epd.display(self.epd.getbuffer(pil_image))
